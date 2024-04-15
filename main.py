f2bc048b529ae6e956542ba6ef0e8f0566b12279
Matias Andrada <matiasandrada5622@gmail.com>
2024-04-15 20:42:06 -0300

    formateo codigo

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ from socioClass import Socio
 
 def main():
     # Creación de objetos a partir de la clase Libro y Socios
-    libro1 = Libro("Aprendiendo Programación I", "Profe Alex",
-                   "2017", "Educativo", "Regular")
-    libro2 = Libro("Aprendiendo Programación II", "Profe Alex",
-                   "2018", "Educativo", "Regular")
-
-    socio1 = Socio(46234324, "Diego", "Grande", "Veneranda",
-                   "DiegoAmarok2024@volsvagen.com", "Vencida")
+    libro1 = Libro("Aprendiendo Programación I", "Profe Alex","2017", "Educativo", "Regular")
+    libro2 = Libro("Aprendiendo Programación II", "Profe Alex","2018", "Educativo", "Regular")
+    
+    socio1 = Socio(46234324, "Diego", "Grande", "Veneranda","DiegoAmarok2024@volsvagen.com", "Vencida")
 
 # Método de acceso al atributo
     print(libro1.autor)
