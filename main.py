0641fbe5a771f73189246623626f06ff1f2a1779
Matias Andrada <matiasandrada5622@gmail.com>
2024-04-15 17:11:06 -0300

    agregando instancia libro 2

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from socioClass import Socio
 
 
 def main():
-    libro1 = Libro("Biblia", "Dios", "0-D.C", "Religion", "Regular")
+    libro1 = Libro("Aprendiendo Programacion I", "Profe Alex",
+                   "2017", "Educativo", "Regular")
+    libro2 = Libro("Aprendiendo Programacion II", "Profe Alex",
+                   "2018", "Educativo", "Regular")
+
     socio1 = Socio(46234324, "Diego", "Grande", "Veneranda",
                    "DiegoAmarok2024@volsvagen.com", "Vencida")
+
     print(libro1.autor)
     libro1.__str__()
 
