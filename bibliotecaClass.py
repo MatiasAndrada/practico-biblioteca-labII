ea230b255893876dfa5ce7cc24724513a5de82f3
Matias Andrada <matiasandrada5622@gmail.com>
2024-04-15 16:00:30 -0300

    cambio de nombre

--- bibliotecaClass.py
+++ bibliotecaClass.py
@@ -1,24 +1,26 @@
-class Libro :
-    def __init__(self,titulo, autor, fecha, genero, estado), titulo, autor, fecha, genero, estado:
+class Libro:
+    def __init__(self, titulo, autor, fecha, genero, estado):
         self.titulo = titulo
         self.autor = autor
         self.fecha = fecha
         self.genero = genero
-        self.estado = estadp
-
+        self.estado = estado
 
     def getTitulo(self):
         return self.Titulo
+
     def getAutor(self):
         return self.Autor
+
     def getFecha(self):
         return self.fecha
+
     def getGenero(self):
         return self.genero
+
     def getEstado(self):
         return self.estado
 
 
-class Socio :
+class Socio:
     def __init__(self):
-
