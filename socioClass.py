771b3ce179c3cfb747dfd0e6e506fe4b6d8e300f
Matias Andrada <matiasandrada5622@gmail.com>
2024-04-15 16:19:02 -0300

    ccreando objeto socio

---
+++ socioClass.py
@@ -0,0 +1,8 @@
+class Socio:
+    def _init_(self, dni, nombre, apellido, direccion, correo, cuota):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        self.direccion = direccion
+        self.correo = correo
+        self.cuota = cuota
