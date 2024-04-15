9545a8da1026dfd98c964a613c0939ad184d86ce
Matias Andrada <matiasandrada5622@gmail.com>
2024-04-15 15:55:22 -0300

    first commit

---
+++ practico_clases.py
@@ -0,0 +1,24 @@
+class Libro :
+    def __init__(self,titulo, autor, fecha, genero, estado), titulo, autor, fecha, genero, estado:
+        self.titulo = titulo
+        self.autor = autor
+        self.fecha = fecha
+        self.genero = genero
+        self.estado = estadp
+
+
+    def getTitulo(self):
+        return self.Titulo
+    def getAutor(self):
+        return self.Autor
+    def getFecha(self):
+        return self.fecha
+    def getGenero(self):
+        return self.genero
+    def getEstado(self):
+        return self.estado
+
+
+class Socio :
+    def __init__(self):
+
